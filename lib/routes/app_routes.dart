733cc61a68cbe9deb00e9b76147a26442ac89f57
Sepi Ananda <sepiananda99@gmail.com>
2026-09-12 23:23:11 +0700

    Add: implement app routes and remove empty tenant dashboard screen;

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../core/constants.dart';
+import '../providers/auth_provider.dart';
+import '../screens/admin/admin_dashboard_screen.dart';
+import '../screens/auth/login_screen.dart';
+import '../screens/owner/owner_dashboard_screen.dart';
+import '../screens/tenant/tenant_dashboard_screen.dart';
