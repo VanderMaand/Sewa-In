af00fd192222a390aca458e21d2e65d167bad72f
Sepi Ananda <sepiananda99@gmail.com>
2026-09-13 00:00:35 +0700

    Add: implement auth provider with necessary imports;

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants.dart';
+import '../../models/user_profile.dart';
+import '../../services/auth_service.dart';
+
+enum AuthStatus { unknown, authenticated, unauthenticated }
