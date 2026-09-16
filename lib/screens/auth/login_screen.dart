9f81c367689e1b357312a30a9023d6eb6de1ae98
Sepi Ananda <sepiananda99@gmail.com>
2026-09-16 22:54:48 +0700

    Remove: unnecessary imports from login and owner dashboard screens;

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,3 @@
 import 'package:flutter/material.dart';
 
 import '../../providers/auth_provider.dart';
-import '../../routes/app_router.dart';
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../providers/auth_provider.dart';
-import '../routes/app_router.dart';
