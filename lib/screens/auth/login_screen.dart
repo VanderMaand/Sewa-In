53ef7e388aa37663b2a3a8942be62ddc825f812c
Sepi Ananda <sepiananda99@gmail.com>
2026-09-16 22:53:04 +0700

    Remove: unnecessary provider import from admin and login screens;

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../providers/auth_ provider.dart';
+import '../../providers/auth_provider.dart';
 import '../../routes/app_router.dart';
